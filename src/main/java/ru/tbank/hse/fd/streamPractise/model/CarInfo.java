cdb9dba6c8faa0eb843902793615947c7b3b8109
toxic-dxd <antoniosmirnov2008@gmail.com>
2024-12-28 00:43:47 +0300

    initial commit

--- src/main/java/ru/tbank/hse/fd/streamPractise/model/CarInfo.java
+++ src/main/java/ru/tbank/hse/fd/streamPractise/model/CarInfo.java
@@ -1,11 +1,8 @@
 package ru.tbank.hse.fd.streamPractise.model;
 
 public class CarInfo {
-    // Название машины
     private String name;
-    // Возраст машины
     private int age;
-    // Количество владельцев
     private int owners;
 
     public CarInfo(String name, int age, int owners) {
